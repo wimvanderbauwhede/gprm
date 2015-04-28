181b3746b543aad0afc238e9d2fe0c18ff4560b0
Wim Vanderbauwhede <Wim.Vanderbauwhede@mail.be>
2015-04-28 11:13:43 +0100

    Building of Haskell code now conditional on OS, static linking on Linux.

---
+++ GPC/build.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use Getopt::Std;
+use Cwd;
+my $wd=cwd();
+my %opts=();
+if (!@ARGV) {$opts{'h'}=1}
+getopts( 'hvCcbi', \%opts );
+
+if ( $opts{'h'} ) {
+die "$0 options:
+    -c: configure
+    -b: build 
+    -i: install
+    -C: clean, then configure
+";
+}
+my $v=$opts{'v'}?'--verbose=2':'';
+my $build=($opts{'b'} || $opts{'c'} || $opts{'C'})  ?1:0;
+my $install=$opts{'i'}?1:0;
+if ($install==1) {$build=0};
+my $c=$opts{'c'}?1:0;
+my $C=$opts{'C'}?1:0;
+$c||=$C;
+my $os=`uname`;chomp $os;
+
+if (-e "./dist/build/gpcc/gpcc"){
+    unlink "./dist/build/gpcc/gpcc";
+}
+
+if ($C) {
+    system("cabal clean");       
+}	
+if ($c) {
+    print "* Cabal configure\n";
+    system("cabal configure"); 
+}
+if ($build) {    
+    print 'cabal build '.$v."\n";
+    system('cabal build '.$v);
+    
+}
+if ($install) {
+    print "* Cabal install\n";
+    print 'cabal install --bindir=../bin'."\n";
+    system('cabal install --bindir=../bin');    
+}
+
+
